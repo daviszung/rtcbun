85519b2972bbe9ed42bffbe1d57a4ded8844b110
daviszung <daviszung@gmail.com>
2023-02-06 18:55:49 -0500

    p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import style from "./styles/App.module.css";
+import { useState } from 'react';
+
+
+const socket = new WebSocket('ws://localhost:8080');
+
+// import { Chat } from './components/chat'
+
 
 function App() {
+
+  const [list, setList] = useState([])
+
+  socket.onmessage = ({ data }) => {
+    console.log(`Message from server ${data}`)
+    setList([...list, data])
+  };
+
   return (
     <div className={style.app} role="main">
       <div>
