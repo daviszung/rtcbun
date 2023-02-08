b34c57ae289ee581a3b60c3fd5fb80dc215f1b09
daviszung <daviszung@gmail.com>
2023-02-08 12:09:16 -0500

    lock user out of joining rooms without a name

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,20 +1,45 @@
-import style from '../styles/components/modal.module.css'
+import style from "../styles/components/modal.module.css"
+
+function checkNameInput() {
+  const input = document.querySelector("#nameInput")
+  return input.value.length > 0;
+}
+
 
 function closeModal() {
   const modal = document.querySelector("#myModal");
-  modal.style.display = 'none'
+  modal.style.display = "none"
+}
+
+async function createRoom() {
+  // send req to http server to get a room number
+}
+
+function joinRoom() {
+
 }
 
 export function Modal() {
+
+  
+
   return (
     <div id="myModal" className={style.modal}>
       <div className={style.nameContainer}>
         <h1 className={style.nameText}>Name:</h1>
-        <input className={style.inputName} maxLength="15"></input>
+        <input id="nameInput" className={style.inputName} maxLength="15"></input>
       </div>
       <div className={style.optionsBtns}>
-        <button className={style.createBtn}>Create a room</button>
-        <button className={style.joinBtn}>Join a room</button>
+        <button id="createBtn" className={style.createBtn} onClick={() => {
+          if (checkNameInput()) {
+            createRoom()
+          }
+        }}>Create a room</button>
+        <button id="joinBtn" className={style.joinBtn} onClick={() => {
+          if (checkNameInput()) {
+            
+          }
+        }}>Join a room</button>
       </div>
     </div>
   )
